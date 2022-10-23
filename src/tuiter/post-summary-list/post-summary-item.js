e66f0b9ea2a9fbbe36f2fd670cc7ae66926c0ca2
Will Del Col <wdelcol@gmail.com>
2022-10-23 17:41:24 -0400

    a6 React.js sp22

--- src/tuiter/post-summary-list/post-summary-item.js
+++ src/tuiter/post-summary-list/post-summary-item.js
@@ -20,7 +20,7 @@ const PostSummaryItem = (
                     <div>{post.title}</div>
                 </div>
                 <div className="col-2">
-                    <img width={70} className="float-end rounded-3" src={post.image}/>
+                    <img width={70} className="float-end rounded-3" src={post.image} alt={"Nothing"}/>
                 </div>
             </div>
         </li>
